5a4875cc05d76968a0b7e1c17b89c878efbee2cd
Alicja Gruca <alicjagruca6@gmail.com>
2023-04-01 09:19:50 +0200

    Initial Commit

---
+++ ImageRecognitionSwiftUI/ImageRecognitionSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRecognitionSwiftUIApp.swift
+//  ImageRecognitionSwiftUI
+//
+//  Created by Alicja Gruca on 01/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageRecognitionSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
